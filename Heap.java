1fb6b4def5cb17d21a314d49d05913c85462b0c1
isabella4t <70291996+isabella4t@users.noreply.github.com>
2023-01-13 18:48:42 -0800

    Update Heap.java

--- Heap.java
+++ Heap.java
@@ -1,9 +1,13 @@
 public class Heap {
     Node root;
     int[] heap;
-
-    public void BinaryHeap(int n){
+    
+    public Heap(int n){
         heap = new int[n];
+    }
+
+    public yHeap(int[] n){
+        heap = int[] n;
     }
     public void insert(int m){
         int[] temp = new int[heap.length+1];
@@ -17,8 +21,16 @@ public class Heap {
 
     }
 
-    public void delete(){
-
+    public void delete(int n){
+         int[] temp = new int[heap.length-1];
+        int acc = 0;
+        for(int i = 0; i<heap.length; i++){
+            if(heap[i] != n){
+                temp[acc] = heap[i];
+                acc++;
+            }
+        }
+        heap = temp;
     }
 
 }
