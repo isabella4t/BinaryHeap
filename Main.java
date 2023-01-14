ed5db9c5b48f9ae8b81e498a6e49871e55e77805
isabella4t <70291996+isabella4t@users.noreply.github.com>
2023-01-13 18:59:06 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 public class Main {
 /*
-htf do we do this?
+Min tree
 
  */
 
-
     public static void main(String[] args){
-
+        
+        int[] m = new int[]{1,2,3,4,5,6};
+        Heap zoo = new Heap(m);
+        m.insert(7);
+        m.delete(2);
     }
+    
+    
 }
